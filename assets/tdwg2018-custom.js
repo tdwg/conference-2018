a5ad4b9581724d3175ca5d070ac746e65c066fee
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2018-02-13 12:31:39 +1100

    Update tdwg2018-custom.js
    Update tdwg2018-custom.js: highlight current page in sidebar

--- assets/tdwg2018-custom.js
+++ assets/tdwg2018-custom.js
@@ -1,6 +1,8 @@
 jQuery.noConflict();
 
 (function($) {
+
+    // Navigation between symposium and workshop pages
     if (location.href.indexOf('/sessions/') !== -1) {
         $('<div/>', {
             class: 'symposia-and-workshops-nav',
@@ -10,7 +12,7 @@ jQuery.noConflict();
             url: '/includes/tdwg2018-symposium-or-workshop-dropdown.html',
             success: function(data) {
                 $('.symposia-and-workshops-nav').html(data);
-                $("ul.dropdown li").hover(function(){
+                $("ul.dropdown li").hover(function() {
 
                     $(this).addClass("hover");
                     $('ul:first',this).css('visibility', 'visible');
@@ -25,8 +27,27 @@ jQuery.noConflict();
                 $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
             }
         });
+    }
 
-
+    // Highlighting of current page in sidebar
+    $('.sidebar li, .sidebar li a').removeClass('current').addClass('link');
+    $('.sidebar li a[title="Go to the TDWG Submissions page"]').removeClass('link').addClass('current')
+      .parent('li').removeClass('link').addClass('current');
+    if (location.href.indexOf('call-for-abstracts') !== -1) {
+        $('.sidebar li a[title="Call for abstracts"]').removeClass('link').addClass('current')
+          .parent('li').removeClass('link').addClass('current');
+    }
+    if (location.href.indexOf('instructions-for-authors') !== -1) {
+        $('.sidebar li a[title="Instructions for authors"]').removeClass('link').addClass('current')
+          .parent('li').removeClass('link').addClass('current');
+    }
+    if (location.href.indexOf('instructions-for-editors') !== -1) {
+        $('.sidebar li a[title="Instructions for editors"]').removeClass('link').addClass('current')
+          .parent('li').removeClass('link').addClass('current');
+    }
+    if (location.href.indexOf('/sessions/') !== -1) {
+        $('.sidebar li a[title="Symposia and workshops"]').removeClass('link').addClass('current')
+          .parent('li').removeClass('link').addClass('current');
     }
 
 }(jQuery));
